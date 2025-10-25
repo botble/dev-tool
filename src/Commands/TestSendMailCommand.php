d691fd11ece38f9e7ed6f9382ee6ec49503430d7
Eng Amr Elgazar <158527211+engamrelgazar@users.noreply.github.com>
2025-10-25 21:56:10 +0300

    Update TestSendMailCommand.php

--- src/Commands/TestSendMailCommand.php
+++ src/Commands/TestSendMailCommand.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace Botble\DevTool\Commands;
+namespace Tiryaq\DevTool\Commands;
 
-use Botble\Base\Facades\EmailHandler;
-use Botble\DevTool\Helper;
+use Tiryaq\Base\Facades\EmailHandler;
+use Tiryaq\DevTool\Helper;
 use Illuminate\Console\Command;
 use Symfony\Component\Console\Attribute\AsCommand;
 
